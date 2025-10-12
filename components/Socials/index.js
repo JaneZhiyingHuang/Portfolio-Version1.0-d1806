ce7b30eaad89dadea57eaf6a88e50b5668312320
JaneWong2023 <ZhiyingHuang2023@outlook.com>
2025-10-13 02:41:48 +0300

    Social: icon added

--- components/Socials/index.js
+++ components/Socials/index.js
@@ -1,13 +1,24 @@
 import React from "react";
+import { Github, Linkedin, Mail } from "lucide-react"; 
 import Button from "../Button";
-
 import yourData from "../../data/portfolio.json";
+
+const iconMap = {
+  Github: <Github className="w-5 h-5 mr-1" />,  
+  LinkedIn: <Linkedin className="w-5 h-5 mr-1" />,
+  Email: <Mail className="w-5 h-5 mr-1" />,
+};
 
 const Socials = ({ className }) => {
   return (
     <div className={`${className} flex flex-wrap mob:flex-nowrap link`}>
       {yourData.socials.map((social, index) => (
-        <Button key={index} onClick={() => window.open(social.link)}>
+        <Button
+          key={index}
+          onClick={() => window.open(social.link)}
+          className="flex items-center"
+        >
+          {iconMap[social.title]}  
           {social.title}
         </Button>
       ))}
